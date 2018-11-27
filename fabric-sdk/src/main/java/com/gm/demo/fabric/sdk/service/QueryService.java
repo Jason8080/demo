59e825cb8c6c855a0c1f504dfa3a93a68afd005f
Jason <admin@gm.com>
2018-11-27 15:04:17 +0800

    想做点什么  .

---
+++ fabric-sdk/src/main/java/com/gm/demo/fabric/sdk/service/QueryService.java
@@ -0,0 +1,14 @@
+package com.gm.demo.fabric.sdk.service;
+
+public class QueryService {
+
+    /**
+     * 查询数据
+     *
+     * @param key
+     * @return
+     */
+    public Object query(String key) {
+        return "";
+    }
+}
