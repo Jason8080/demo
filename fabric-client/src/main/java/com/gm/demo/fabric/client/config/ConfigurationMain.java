c1bdd7fbec652d54ec0333ea1505ceae7e8ba1fc
Jason <admin@gm.com>
2019-01-28 12:50:49 +0800

    init.

--- fabric-client/src/main/java/com/gm/demo/fabric/client/config/ConfigurationMain.java
+++ fabric-client/src/main/java/com/gm/demo/fabric/client/config/ConfigurationMain.java
@@ -3,6 +3,7 @@ package com.gm.demo.fabric.client.config;
 import com.gm.demo.fabric.client.config.order.Order;
 import com.gm.demo.fabric.client.config.peer.Peer;
 import com.gm.demo.fabric.client.config.user.Admin;
+import org.hyperledger.fabric.sdk.BlockchainInfo;
 import org.hyperledger.fabric.sdk.Channel;
 import org.hyperledger.fabric.sdk.HFClient;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
@@ -18,6 +19,8 @@ public class ConfigurationMain {
 
     public static final String js_channel_name = "jschannel";
 
+    @Autowired
+    Order order1;
     @Autowired
     Peer peer0Org1;
     @Autowired
@@ -36,17 +39,18 @@ public class ConfigurationMain {
     }
 
     @Bean
-    public Channel getChannel(HFClient client, Order order) throws Exception {
+    public Channel getChannel(HFClient client) throws Exception {
         Channel js = client.newChannel(js_channel_name);
-        js.addOrderer(client.newOrderer(order.getName(), order.getLoc()));
+        js.addOrderer(client.newOrderer(order1.getName(), order1.getLoc()));
         js.addPeer(client.newPeer(peer0Org1.getName(), peer0Org1.getLoc()));
         js.addEventHub(client.newEventHub(peer0Org1.getName(), peer0Org1.getEventLoc()));
-        return js;
+        return js.initialize();
     }
 
     @Bean
     public Void main(Channel js) throws Exception {
-        js.initialize();
+        BlockchainInfo info = js.queryBlockchainInfo();
+        System.out.println(info);
         return null;
     }
 }
