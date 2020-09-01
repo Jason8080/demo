2b9a0c5e90d14a8fc3320518b9426dc85a29bca1
Jason <xiaoku13141@163.com>
2020-09-01 20:24:57 +0800

    调整脚手架结构.

--- nacos-root/nacos-pm/pm-provider/src/main/java/com/gm/demo/nacos/server/provider/NacosProviderApp.java
+++ nacos-root/nacos-pm/pm-provider/src/main/java/com/gm/demo/nacos/server/provider/NacosProviderApp.java
@@ -13,7 +13,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
