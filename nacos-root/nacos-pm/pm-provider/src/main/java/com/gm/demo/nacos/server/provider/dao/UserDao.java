00f54ac65b718d5c4588047813166bf910f2be92
Jason <xiaoku13141@163.com>
2020-09-03 16:53:11 +0800

    dao可以不使用 .

---
+++ nacos-root/nacos-pm/pm-provider/src/main/java/com/gm/demo/nacos/server/provider/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.gm.demo.nacos.server.provider.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 用户持久层
+ *
+ * @author Timi.lee
+ * @date 2020/9/3 (周四)
+ */
+@Repository
+public class UserDao {
+}
