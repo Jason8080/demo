c4f070179af232594303eb5bb6167eaef190eb29
Jason <xiaoku13141@163.com>
2020-09-03 20:44:52 +0800

    技术异常完善.

--- nacos-root/common/src/main/java/com/gm/demo/nacos/server/common/ex/SkillException.java
+++ nacos-root/common/src/main/java/com/gm/demo/nacos/server/common/ex/SkillException.java
@@ -7,22 +7,34 @@ package com.gm.demo.nacos.server.common.ex;
  * @date 2020/9/3 (周四)
  */
 public class SkillException extends RuntimeException {
-    public SkillException() {
+
+    private Integer code;
+
+    public Integer getCode() {
+        return code;
     }
 
-    public SkillException(String message) {
+    public SkillException(Integer code) {
+        this.code = code;
+    }
+
+    public SkillException(String message, Integer code) {
         super(message);
+        this.code = code;
     }
 
-    public SkillException(String message, Throwable cause) {
+    public SkillException(String message, Throwable cause, Integer code) {
         super(message, cause);
+        this.code = code;
     }
 
-    public SkillException(Throwable cause) {
+    public SkillException(Throwable cause, Integer code) {
         super(cause);
+        this.code = code;
     }
 
-    public SkillException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public SkillException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, Integer code) {
         super(message, cause, enableSuppression, writableStackTrace);
+        this.code = code;
     }
 }
