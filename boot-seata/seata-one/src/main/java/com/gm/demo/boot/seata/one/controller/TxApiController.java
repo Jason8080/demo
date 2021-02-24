5dd2fb53653c1caa50faa702de756b972b4ec441
Timi <xiaoku13141@163.com>
2021-02-24 13:48:07 +0800

    gm-demo: seata事务单节点可行实验代码

--- boot-seata/seata-one/src/main/java/com/gm/demo/boot/seata/one/controller/TxApiController.java
+++ boot-seata/seata-one/src/main/java/com/gm/demo/boot/seata/one/controller/TxApiController.java
@@ -1,6 +1,8 @@
 package com.gm.demo.boot.seata.one.controller;
 
+import cn.gmlee.tools.base.enums.XTime;
 import cn.gmlee.tools.base.mod.JsonResult;
+import cn.gmlee.tools.base.util.TimeUtil;
 import com.gm.demo.boot.seata.main.api.entity.Tx;
 import com.gm.demo.boot.seata.main.api.enums.MicroConstant;
 import com.gm.demo.boot.seata.one.service.TxService;
@@ -24,6 +26,7 @@ public class TxApiController {
     @PostMapping
     @ApiOperation("")
     public JsonResult come(String str) {
+        System.out.println("================"+TimeUtil.getCurrentDatetime(XTime.LONG_DATE_PATTERN_WITH_MS_LINE));
         Tx tx = new Tx(str);
         txService.come(tx);
         return JsonResult.OK;
