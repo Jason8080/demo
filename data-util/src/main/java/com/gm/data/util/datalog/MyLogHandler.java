efbaaeaeb2b1b21dd386d3ca7fedd2bdbdca1599
Timi <xiaoku13141@163.com>
2021-04-08 14:47:51 +0800

    可用版本: datalog

--- data-util/src/main/java/com/gm/data/util/datalog/MyLogHandler.java
+++ data-util/src/main/java/com/gm/data/util/datalog/MyLogHandler.java
@@ -1,8 +1,9 @@
 package com.gm.data.util.datalog;
 
 import cn.gmlee.tools.datalog.interceptor.AbstractDatalogInterceptorHandler;
+import cn.gmlee.tools.mybatis.dao.IBatisDao;
 import com.gm.data.util.dao.entity.MyLog;
-import com.gm.data.util.dao.mapper.MyLogMapper;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,12 +18,13 @@ import javax.annotation.Resource;
 public class MyLogHandler extends AbstractDatalogInterceptorHandler<MyLog> {
 
     @Resource
-    MyLogMapper myLogMapper;
+    SqlSessionFactory sqlSessionFactory;
 
     Logger logger = LoggerFactory.getLogger(MyLogHandler.class);
     @Override
     public boolean commit(MyLog myLog) {
-        myLogMapper.insert(myLog);
+        IBatisDao<MyLog> iBatisDao = new IBatisDao<>(sqlSessionFactory);
+        iBatisDao.execute(myLog, "cn.gmlee.tools.datalog.dao.mapper.DatalogMapper.insert");
         return true;
     }
 }
