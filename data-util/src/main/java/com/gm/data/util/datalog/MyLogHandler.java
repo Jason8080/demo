cba3e1f7f35f2509cf0336dd31a998e011ae46b3
Timi <xiaoku13141@163.com>
2021-04-09 16:34:30 +0800

    可用版本: datalog

--- data-util/src/main/java/com/gm/data/util/datalog/MyLogHandler.java
+++ data-util/src/main/java/com/gm/data/util/datalog/MyLogHandler.java
@@ -1,15 +1,17 @@
 package com.gm.data.util.datalog;
 
+import cn.gmlee.tools.base.util.JdbcUtil;
 import cn.gmlee.tools.datalog.interceptor.AbstractDatalogInterceptorHandler;
-import cn.gmlee.tools.mybatis.dao.IBatisDao;
 import com.gm.data.util.dao.entity.MyLog;
 import com.gm.data.util.dao.mapper.MyLogMapper;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Jas°
@@ -22,14 +24,17 @@ public class MyLogHandler extends AbstractDatalogInterceptorHandler<MyLog> {
     MyLogMapper myLogMapper;
 
     @Resource
-    SqlSessionFactory sqlSessionFactory;
+    DataSource dataSource;
 
     Logger logger = LoggerFactory.getLogger(MyLogHandler.class);
+
     @Override
     public boolean commit(MyLog myLog) {
-        myLogMapper.insert(myLog);
-//        IBatisDao<MyLog> iBatisDao = new IBatisDao<>(sqlSessionFactory);
-//        iBatisDao.execute(myLog, "cn.gmlee.tools.datalog.dao.mapper.DatalogMapper.insert");
+        List<Map<String, Object>> result = JdbcUtil.exec(
+                dataSource,
+                myLog.getDatalogSelectSql()
+        );
+        System.out.println(result);
         return true;
     }
 }
