8e1793faa26e0db1e11f71746b7eb85f9ff8d84c
Timi <xiaoku13141@163.com>
2021-04-29 18:04:00 +0800

    可用版本: 同事支持指定属性名称和正则脱敏

--- tools-request/src/main/java/com/gm/demo/tools/request/controller/vo/Demo.java
+++ tools-request/src/main/java/com/gm/demo/tools/request/controller/vo/Demo.java
@@ -2,6 +2,9 @@ package com.gm.demo.tools.request.controller.vo;
 
 import lombok.Data;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Jas°
  * @date 2021/4/25 (周日)
@@ -11,4 +14,7 @@ public class Demo {
     private Long id;
     private String name;
     private String number;
+    private List<Demo> demos;
+    private List<String> numbers;
+    private List<Map<String, String>> maps;
 }
