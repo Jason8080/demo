11cc3b7a629e199fcff2c9ff303b2c05ad85345f
Timi <xiaoku13141@163.com>
2021-09-18 17:31:44 +0800

    测试rbac 起步依赖

---
+++ rbac-jedis/src/main/java/cn/gmlee/demo/rbac/jedis/App.java
@@ -0,0 +1,20 @@
+package cn.gmlee.demo.rbac.jedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * .
+ *
+ * @author Jas°
+ * @date 2021/9/18 (周六)
+ */
+@SpringBootApplication
+@ComponentScan({"cn.hll.css.dependencies", "cn.gmlee.demo.rbac.jedis"})
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
