c5534e3c23d390c06e48887e0c00e44a474507d4
gm.li <gm.li@huolala.cn>
2022-11-25 15:55:59 +0800

    测试事务有效

---
+++ dt-demo/src/main/java/cn/gmlee/dt/demo/controller/TestController.java
@@ -0,0 +1,42 @@
+package cn.gmlee.dt.demo.controller;
+
+
+import cn.gmlee.dt.demo.service.TabService;
+import cn.gmlee.dt.demo.service.impl.ExTabService;
+import cn.hll.tools.base.anno.ApiPrint;
+import cn.hll.tools.base.mod.JsonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Jas°
+ * @since 2022-11-22
+ */
+@Validated
+@RestController
+@Api(tags = {"测试"})
+@RequestMapping("test")
+public class TestController {
+     @Resource
+     private ExTabService exTabService;
+
+     @ApiOperation(value = "批处理")
+     @ApiPrint(value = "批处理")
+     @PostMapping(value = "batch")
+     public JsonResult batch(@RequestBody List<Long> ids) {
+         exTabService.batch(ids);
+           return JsonResult.OK;
+      }
+}
---
+++ dt-demo/src/main/java/cn/gmlee/dt/demo/service/impl/ExTabService.java
@@ -0,0 +1,40 @@
+package cn.gmlee.dt.demo.service.impl;
+
+import cn.gmlee.dt.demo.dao.mapper.TabMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author Jas °
+ * @since 2022 -11-22
+ */
+@Service
+public class ExTabService {
+
+    @Resource
+    private TabMapper tabMapper;
+
+
+    /**
+     * Batch.
+     *
+     * @param ids the ids
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public void batch(List<Long> ids) {
+        for (int i = 0; i < ids.size(); i++){
+            if(i == ids.size() - 1){
+                int ex = i / 0;
+                System.out.println(ex);
+            }
+            tabMapper.deleteById(ids.get(i));
+        }
+    }
+}
