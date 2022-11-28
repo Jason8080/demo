40fea262896bd2c0d5851214fb447597bfafef15
gm.li <gm.li@huolala.cn>
2022-11-28 17:59:04 +0800

    启动自定义事务管理器2

--- dt-demo/src/main/java/cn/gmlee/dt/demo/DtApp.java
+++ dt-demo/src/main/java/cn/gmlee/dt/demo/DtApp.java
@@ -1,15 +1,15 @@
 package cn.gmlee.dt.demo;
 
-import org.springframework.boot.SpringApplication;
+import cn.gmlee.cc.framework.launcher.EnableServer;
+import cn.gmlee.dt.spring.boot.starter.launcher.DtSpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * The type Main app.
  */
-@SpringBootApplication(exclude = {
-//        DtSpringTransactionAutoConfiguration.class,
-})
+@EnableServer
+@SpringBootApplication
 @ComponentScan({"cn.gmlee.dt", "cn.hll.tools"})
 public class DtApp {
 
@@ -19,6 +19,6 @@ public class DtApp {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        SpringApplication.run(DtApp.class, args);
+        DtSpringApplication.run(DtApp.class, args);
     }
 }
