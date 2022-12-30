539a20d22dfcaf9b913aa23767a9e15b28fdfb64
gm.li <gm.li@huolala.cn>
2022-12-30 17:55:54 +0800

    5.3.0 分片算法有效

--- tools-sharding/src/main/java/com/gm/demo/tools/sharding/dao/entity/Tab.java
+++ tools-sharding/src/main/java/com/gm/demo/tools/sharding/dao/entity/Tab.java
@@ -11,7 +11,7 @@ import java.util.Date;
  * @date 2021/6/10 (周四)
  */
 @Data
-@TableName("sharding_tab")
+@TableName("ts_tab")
 public class Tab implements Serializable {
     private Long id;
     private String column1;
