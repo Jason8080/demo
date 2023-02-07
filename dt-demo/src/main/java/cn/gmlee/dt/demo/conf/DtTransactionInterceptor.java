612120d9e1a1e88fe4902a1921a92d50f2ba5174
gm.li <gm.li@huolala.cn>
2023-02-07 11:18:38 +0800

    重写事务拦截器切面

---
+++ dt-demo/src/main/java/cn/gmlee/dt/demo/conf/DtTransactionInterceptor.java
@@ -0,0 +1,26 @@
+package cn.gmlee.dt.demo.conf;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.interceptor.TransactionAttribute;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
+/**
+ * 事务拦截器.
+ */
+public class DtTransactionInterceptor extends TransactionInterceptor {
+
+    @Override
+    protected TransactionInfo createTransactionIfNecessary(PlatformTransactionManager tm, TransactionAttribute txAttr, String joinpointIdentification) {
+        return super.createTransactionIfNecessary(tm, txAttr, joinpointIdentification);
+    }
+
+    @Override
+    protected void commitTransactionAfterReturning(TransactionInfo txInfo) {
+//        super.commitTransactionAfterReturning(txInfo);
+    }
+
+    @Override
+    protected void completeTransactionAfterThrowing(TransactionInfo txInfo, Throwable ex) {
+        super.completeTransactionAfterThrowing(txInfo, ex);
+    }
+}
