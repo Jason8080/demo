8b7a151b1ec6251131ebe37bf66d52a76ba49282
liqing <xiaoku13141@163.com>
2024-04-10 14:15:58 +0800

    el表达式验收通过

--- k8s_app_demo/k8s_app_demo/src/main/java/cn/gmlee/demo/k8s/app/demo/controller/vo/Noe.java
+++ k8s_app_demo/k8s_app_demo/src/main/java/cn/gmlee/demo/k8s/app/demo/controller/vo/Noe.java
@@ -4,15 +4,17 @@ import cn.gmlee.tools.base.anno.Check;
 import cn.gmlee.tools.base.anno.El;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Data
 @Check
 public class Noe implements Serializable {
-    @El(conditions = "${a}==${b}", value = "${key}!='' && ${c}!=0")
     private String b;
     private String a;
     private Integer c;
     private String key;
+    @NotEmpty(message = "值内容不可以为空")
+    @El(conditions = "${a}==${b}", value = "${key}!='' && ${value}!=null && ${value}!=''", message = "值内容是空")
     private String value;
 }
